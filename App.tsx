68ab2692c9a176c2bf2d109357328852f8fc1bdf
tay <phuonggteyy@gmail.com>
2024-04-04 14:45:58 +0700

    setup project

--- App.tsx
+++ App.tsx
@@ -25,6 +25,8 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import CustomIcon from './src/components/CustomIcon';
+
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
@@ -72,6 +74,7 @@ function App(): React.JSX.Element {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <Header />
+        <CustomIcon name="search" size={25}/>
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
